aa4a23e00e2416b7fc3020d2186648c9bd51aebc
Simon Thordal <simonthordal@gmail.com>
2021-12-05 14:02:31 +0100

    add rolling endpoint

--- api/api.py
+++ api/api.py
@@ -3,3 +3,7 @@ import hug
 @hug.get("/hello_world")
 def hello_world():
 	return "hello world"
+
+@hug.get("/rolling-five-days/{countryterritoryCode}")
+def rolling_five_days():
+	pass
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,3 +6,8 @@ class TestApi(unittest.TestCase):
 	def test_initial_route(self):
 		result = hug.test.get(api, "hello_world")
 		self.assertEqual(result.status, hug.HTTP_200)
+
+	def test_rolling_endpoint(self):
+		code = "es"
+		result = hug.test.get(api, "/rolling-five-days/{}".format(code))
+		self.assertEqual(result.status, hug.HTTP_200)
