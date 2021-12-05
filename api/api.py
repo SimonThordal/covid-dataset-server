27cceb9162ff779f6dee6f4b500e2931656a466c
Simon Thordal <simonthordal@gmail.com>
2021-12-05 19:02:02 +0100

    Return the counts from the API

--- api/api.py
+++ api/api.py
@@ -1,9 +1,29 @@
 import hug
+import json
+from pathlib import Path
+import os
+
+def load_counts(country_code: str) -> list:
+    datapath = Path(os.path.abspath(os.path.dirname(__file__))) / "data"
+    filename = Path(country_code).with_suffix(".json")
+    path = datapath / filename
+    with path.open() as fl:
+        return json.load(fl)
+
+
+def get_country(country_code: str) -> str:
+    pass
+
 
 @hug.get("/hello_world")
 def hello_world():
-	return "hello world"
+    return "hello world"
+
 
 @hug.get("/rolling-five-days/{countryterritoryCode}")
-def rolling_five_days():
-	return []
+def rolling_five_days(countryterritoryCode: hug.types.text):
+    data = dict();
+    data['counts'] = load_counts(countryterritoryCode)
+    data['country_code'] = countryterritoryCode
+    data['country'] = get_country(countryterritoryCode)
+    return data
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,26 @@
 import unittest
 import hug
-import json
 from .context import api
 
-class TestApi(unittest.TestCase):
-	def test_initial_route(self):
-		result = hug.test.get(api, "hello_world")
-		self.assertEqual(result.status, hug.HTTP_200)
 
-	def test_rolling_endpoint(self):
-		# When calling the endpoint with a valid country code
-		code = "es"
-		result = hug.test.get(api, "/rolling-five-days/{}".format(code))
-		# The request is succesful
-		self.assertEqual(result.status, hug.HTTP_200)
-		# The response is JSON
-		self.assertEqual(result.headers_dict['content-type'], 'application/json; charset=utf-8')
-		# The response contains data for the given company
-		self.assertEqual(len(result.data), 1)
+class TestApi(unittest.TestCase):
+    def test_initial_route(self):
+        result = hug.test.get(api, "hello_world")
+        self.assertEqual(result.status, hug.HTTP_200)
+
+    def test_rolling_endpoint(self):
+        # When calling the endpoint with a valid country code
+        code = "ESP"
+        result = hug.test.get(api, "/rolling-five-days/{}".format(code))
+        # The request is succesful
+        self.assertEqual(result.status, hug.HTTP_200)
+        # The response is JSON
+        self.assertEqual(result.headers_dict['content-type'], 'application/json; charset=utf-8')
+        # The response contains data for the given country
+        self.assertIn('counts', result.data)
+        self.assertGreater(len(result.data['counts']), 0)
+        # The response contains the expected keys
+        self.assertIn('country', result.data)
+        self.assertEqual('Spain', result.data['country'])
+        self.assertIn('country_code', result.data)
+        self.assertEqual('ESP', result.data['country_code'])
